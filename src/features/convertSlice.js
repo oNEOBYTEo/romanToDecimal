7c03731a8d3eb3b5ccd124b9202683e71b2fce5d
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-06-15 19:44:18 -0500

    finish project

--- src/features/convertSlice.js
+++ src/features/convertSlice.js
@@ -1,20 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice } from '@reduxjs/toolkit';
+import { sumRoman, sumInt } from '../helpers/filters';
 export const convertSlice = createSlice({
   name: 'convert',
   initialState: {
     value: '',
   },
   reducers: {
-    romanToDecimal: (state) => {
-      state.value = '123456789';
+    romanToDecimal: (state, action) => {
+      if (
+        parseInt(sumRoman(action.payload)) < 1 ||
+        parseInt(sumRoman(action.payload)) > 3999
+      ) {
+        alert('no puedes llegar a mas de MMMCMXCIX(3999)');
+        state.value = '';
+      } else {
+        state.value = sumRoman(action.payload);
+      }
     },
-    decimalToRoman: (state) => {
-      state.value = '12345';
-    }
+    decimalToRoman: (state, action) => {
+      if (parseInt(action.payload) < 1 || parseInt(action.payload) > 3999) {
+        alert('no puedes llegar a mas de 3999 (MMMCMXCIX), ni menos de 1');
+        state.value = '';
+      } else {
+        state.value = sumInt(parseInt(action.payload));
+      }
+    },
   },
-})
+});
 
-export const { romanToDecimal, decimalToRoman } = convertSlice.actions
+export const { romanToDecimal, decimalToRoman } = convertSlice.actions;
 
-export default convertSlice.reducer
+export default convertSlice.reducer;
